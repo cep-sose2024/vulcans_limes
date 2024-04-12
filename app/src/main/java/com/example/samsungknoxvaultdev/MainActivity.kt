0a602c5542ad88b02b7cc49c0e11d6bf75cc9272
noah <3001838@stud.hs-mannheim.de>
2024-04-12 14:02:21 +0200

    Gruppe testet gemeinsam

--- app/src/main/java/com/example/samsungknoxvaultdev/MainActivity.kt
+++ app/src/main/java/com/example/samsungknoxvaultdev/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
-                    //Noah testet. Funktioniert es?
-                    //Ja tut es
+                    //Gruppe testet gemeinsam
                 }
             }
         }

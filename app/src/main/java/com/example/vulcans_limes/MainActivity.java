705266a6770a9221038e2f00d58250871fafab3a
noah-pe <3001838@stud.hs-mannheim.de>
2024-05-06 19:16:53 +0200

    implemented create and load key

--- app/src/main/java/com/example/vulcans_limes/MainActivity.java
+++ app/src/main/java/com/example/vulcans_limes/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.vulcans_limes;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -8,6 +10,7 @@ import android.provider.MediaStore;
 import android.security.keystore.KeyGenParameterSpec;
 import android.security.keystore.KeyProperties;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -132,9 +135,30 @@ public class MainActivity extends AppCompatActivity {
         //When load key button is pressed
         loadButton.setOnClickListener(v -> {
             try{
-                //TODO
 
-                Snackbar.make(v, "The key was successfully loaded!", Snackbar.LENGTH_SHORT).show();
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Name the ID of the key to load:");
+                final EditText input = new EditText(this);
+                builder.setView(input);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String keyId = input.getText().toString();
+                        cryptoManager.setKEY_NAME(keyId);
+                        Snackbar.make(v, "The key with ID \"" + keyId + "\" was successfully loaded!", Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+
+
+
             } catch (Exception e){
                 e.printStackTrace();
             }
@@ -143,9 +167,29 @@ public class MainActivity extends AppCompatActivity {
         //When create key button is pressed
         createButton.setOnClickListener(v -> {
             try{
-                //TODO
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Name the ID of the key to create:");
+                final EditText input = new EditText(this);
+                builder.setView(input);
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String keyId = input.getText().toString();
+                        boolean generatedSuccess = cryptoManager.genKey(keyId);
+                        if(generatedSuccess){
+                            Snackbar.make(v, "The key with ID \"" + keyId + "\" was successfully created!", Snackbar.LENGTH_SHORT).show();
+                        }
 
-                Snackbar.make(v, "A key was successfully created!", Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+
             } catch (Exception e){
                 e.printStackTrace();
             }
